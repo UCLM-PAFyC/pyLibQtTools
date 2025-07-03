bb7d5609041825648135a9de387e049869a7637a
DHL <david.hernandez@uclm.es>
2025-07-03 19:33:01 +0200

    .

--- QProcessDialog.py
+++ QProcessDialog.py
@@ -10,6 +10,10 @@ import sys, os
 import re
 import time
 
+here = os.path.dirname(__file__)
+sys.path.append(os.path.join(here, '.'))
+
+import defs_qprocess
 
 class QProcessDialog(QDialog):
     """Employee dialog."""
@@ -22,9 +26,16 @@ class QProcessDialog(QDialog):
         # loadUi("lib/InstrumentsDialog.ui", self)
         self.process = None
         self.terminatePushButton.clicked.connect(self.terminate)
+        self.number_of_steps = None
 
     def process_state_message(self, s):
         self.processStatePlainTextEdit.appendPlainText(s)
+
+    def set_progress_range(self, min, max):
+        self.progressBar.setEnabled(True)
+        self.progressBar.setVisible(True)
+        self.progressBar.setRange(min, max)
+        return
 
     def standard_error_message(self, s):
         self.standardErrorPlainTextEdit.appendPlainText(s)
@@ -35,10 +46,13 @@ class QProcessDialog(QDialog):
     def start_process(self,
                       program,
                       arguments,
-                      progress_percent_parsing = "Total complete: (\d+)%"):
+                      string_to_read_number_of_steps = defs_qprocess.STRING_TO_PUBLISH_THE_NUMBER_OF_STEPS_DEFAULT,
+                      string_to_read_completed_steps_percentage = defs_qprocess.STRING_TO_PUBLISH_COMPLETED_STEPS_PERCENTAGE_DEFAULT):
         if self.process is None:  # No process running.
-            self.progress_percent_parsing = progress_percent_parsing
-            self.progress_re = re.compile(progress_percent_parsing)
+            self.string_to_read_number_of_steps = string_to_read_number_of_steps
+            self.string_to_read_completed_steps_percentage = string_to_read_completed_steps_percentage
+            self.progress_number_of_steps_re = re.compile(string_to_read_number_of_steps)
+            self.progress_completed_steps_percentage_re = re.compile(string_to_read_completed_steps_percentage)
             self.processStatePlainTextEdit.clear()
             self.standardErrorPlainTextEdit.clear()
             self.standardOutputPlainTextEdit.clear()
@@ -48,7 +62,8 @@ class QProcessDialog(QDialog):
             self.process.readyReadStandardError.connect(self.handle_stderr)
             self.process.stateChanged.connect(self.handle_state)
             self.process.finished.connect(self.process_finished)  # Clean up once complete.
-            self.progressBar.setRange(0, 100)
+            self.progressBar.setEnabled(False)
+            self.progressBar.setVisible(False)
             self.start_time = time.time()
             self.process.start(program, arguments)
 
@@ -61,9 +76,17 @@ class QProcessDialog(QDialog):
         data = self.process.readAllStandardOutput()
         stdout = bytes(data).decode("utf8")
         # Extract progress if it is in the data.
-        progress = self.simple_percent_parser(stdout)
-        if progress:
-            self.progressBar.setValue(progress)
+        if not self.progressBar.isEnabled():
+            min, max = self.progress_range_parser(stdout)
+            if max > 0:
+                self.progressBar.setEnabled(True)
+                self.progressBar.setVisible(True)
+                self.progressBar.setRange(min, max)
+                self.number_of_steps = max
+        else:
+            progress = self.progress_percent_parser(stdout)
+            if progress:
+                self.progressBar.setValue(progress)
         self.standard_output_message(stdout)
 
     def handle_state(self, state):
@@ -81,17 +104,31 @@ class QProcessDialog(QDialog):
         elapsed_time = self.end_time - self.start_time
         str_elapsed_time_in_seconds = ("\nElapsed Time: {:.3f} seconds".format(elapsed_time))
         self.process_state_message(str_elapsed_time_in_seconds)
+        if self.number_of_steps:
+            self.progressBar.setValue(self.number_of_steps)
         self.process = None
 
-    def simple_percent_parser(self, output):
+    def progress_percent_parser(self, output):
         """
         Matches lines using the progress_re regex,
         returning a single integer for the % progress.
         """
-        m = self.progress_re.search(output)
-        if m:
-            pc_complete = m.group(1)
+        match = self.progress_completed_steps_percentage_re.search(output)
+        if match:
+            pc_complete = match.group(1)
             return int(pc_complete)
+
+    def progress_range_parser(self, output):
+        """
+        Matches lines using the progress_re regex,
+        returning a single integer for the % progress.
+        """
+        min = 0
+        max = 0
+        match = self.progress_number_of_steps_re.search(output)
+        if match:
+            max = int(match.group(1))
+        return min, max
 
     def terminate(self):
         if self.process:
---
+++ defs_qprocess.py
@@ -0,0 +1,7 @@
+# authors:
+# David Hernandez Lopez, david.hernandez@uclm.es
+
+ARGPARSER_TAG_STRING_TO_PUBLISH_THE_NUMBER_OF_STEPS = "string_to_publish_number_of_steps"
+STRING_TO_PUBLISH_THE_NUMBER_OF_STEPS_DEFAULT = "Number of steps: (\d+)"
+ARGPARSER_TAG_STRING_TO_PUBLISH_COMPLETED_STEPS_PERCENTAGE = "string_to_publish_completed_steps_percentage"
+STRING_TO_PUBLISH_COMPLETED_STEPS_PERCENTAGE_DEFAULT = "Total complete: (\d+)%"
