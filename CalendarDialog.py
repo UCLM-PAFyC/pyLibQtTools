a1a658e1b7a1b092c6e54be12807dece09ed1a71
DHL <david.hernandez@uclm.es>
2025-10-18 11:59:00 +0200

    .

---
+++ CalendarDialog.py
@@ -0,0 +1,16 @@
+
+from PyQt5.QtWidgets import QDialog, QCalendarWidget
+from PyQt5.QtCore import QDate
+
+class CalendarDialog(QDialog):
+    def __init__(self, parent, title = None, date = None):
+        super().__init__(parent)
+        if not title:
+            title = "Select date"
+        self.setWindowTitle(title)
+        self.calendar = QCalendarWidget(self)
+        if not date:
+            date = QDate.currentDate()
+        self.calendar.setSelectedDate(date)
+        self.resize(300, 300)
+        self.calendar.resize(300, 300)
