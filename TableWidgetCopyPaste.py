10de67d5c9937f33d046efb71691c9f29a1a13ea
DHL <david.hernandez@uclm.es>
2026-03-12 10:55:46 +0100

    .

---
+++ TableWidgetCopyPaste.py
@@ -0,0 +1,36 @@
+# authors:
+# David Hernandez Lopez, david.hernandez@uclm.es
+
+from PyQt5.QtWidgets import (QTableWidgetItem, QAbstractScrollArea)
+from PyQt5.QtWidgets import (QTableWidget, QApplication, QSizePolicy)
+from PyQt5.QtCore import Qt
+
+class TableWidgetCopyPaste(QTableWidget):
+    """
+    this class extends QTableWidget
+    * supports copying multiple cell's text onto the clipboard
+    * formatted specifically to work with multiple-cell paste into programs
+      like google sheets, excel, or numbers
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def keyPressEvent(self, event):
+        super().keyPressEvent(event)
+        if event.key() == Qt.Key_C and (event.modifiers() & Qt.ControlModifier):
+            self.copied_cells = sorted(self.selectedIndexes())
+            copy_text = ''
+            max_column = self.copied_cells[-1].column()
+            for c in self.copied_cells:
+                copy_text += self.item(c.row(), c.column()).text()
+                if c.column() == max_column:
+                    copy_text += '\n'
+                else:
+                    copy_text += '\t'
+            QApplication.clipboard().setText(copy_text)
+        elif event.key() == Qt.Key_V and (event.modifiers() & Qt.ControlModifier):
+            r = self.currentRow() - self.copied_cells[0].row()
+            c = self.currentColumn() - self.copied_cells[0].column()
+            for cell in self.copied_cells:
+                self.setItem(cell.row() + r, cell.column() + c, QTableWidgetItem(cell.data()))
